5f5d85c2ee33d4806485dc121920b948768e33ce
mdayan96386 <ayan96386@gmail.com>
2025-09-04 01:12:47 +0530

    forth commit

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
+  server:{
+    proxy:{
+      '/api': {
+        changeOrigin: true,
+        target: 'https://e-commerce-tdq7.onrender.com/',
+        secure: false,
+      }
+    }
+  }
 })
