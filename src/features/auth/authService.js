d2cc92c0a47405c33c224295d796db8949fd4c4c
mdayan96386 <ayan96386@gmail.com>
2025-09-04 00:38:30 +0530

    second commit

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
+const base_url = 'https://e-commerce-tdq7.onrender.com'
+
 export const register = async (formData) => {
-  const response = await axios.post("api/auth/register", formData);
+  const response = await axios.post(`${base_url}api/auth/register`, formData);
   localStorage.setItem("user", JSON.stringify(response.data));
   return response.data;
 };
 
 export const login = async (formData) => {
-    const response = await axios.post("api/auth/login", formData);
+    const response = await axios.post(`${base_url}api/auth/login`, formData);
     localStorage.setItem("user", JSON.stringify(response.data));
     return response.data;
   };
